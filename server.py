2172f234869d7a85f38eafb513117b2d3333850d
hcevat1234 <huseyincevatugurluoglu@gmail.com>
2026-08-26 23:13:45 +0300

    Change model name from qwen to openai GPT

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def block_scanners():
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
-MODEL_NAME = "qwen/qwen3.8-27b"
+MODEL_NAME = "openai/gpt-oss-120b"
 ADMIN_USER = "HscAdmin"
 ADMIN_PASS = "4876Hsc487634544800"
 
